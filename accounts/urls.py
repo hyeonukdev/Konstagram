cbcdeb001d02cea3f9775e802d845c80c9aa37f8
hyeonukdev <lego7407@gmail.com>
2019-07-03 08:51:40 +0900

    url수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,3 +8,8 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', register, name='register'),
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
