a26af36d20e491d52ea929232ab7d97b3bab4b62
hyeonukdev <lego7407@gmail.com>
2019-07-04 01:36:51 +0900

    수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', register, name='register'),
 ]
 
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf import settings
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photo/urls.py
+++ photo/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('update/<int:pk>/', PhotoUpdateView.as_view(), name='photo_update'),
 ]
 
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf import settings
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
